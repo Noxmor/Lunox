2cf0053e36ab399212af352cace6a3cdd42d7545
Noxmor <lord.noxmor@gmail.com>
2023-08-07 20:39:46 +0200

    Added blocker masks for slider pieces

--- lunox/src/lunox/boards/bitboard.c
+++ lunox/src/lunox/boards/bitboard.c
@@ -9,6 +9,9 @@ Bitboard black_pawn_attacks[LNX_BOARD_WIDTH * LNX_BOARD_HEIGHT];
 Bitboard knight_attacks[LNX_BOARD_WIDTH * LNX_BOARD_HEIGHT];
 Bitboard king_attacks[LNX_BOARD_WIDTH * LNX_BOARD_HEIGHT];
 
+Bitboard bishop_blocker_masks[LNX_BOARD_WIDTH * LNX_BOARD_HEIGHT];
+Bitboard rook_blocker_masks[LNX_BOARD_WIDTH * LNX_BOARD_HEIGHT];
+
 static void init_white_pawn_push_bitboard(Square square)
 {
     Bitboard pawn_bitboard = LNX_BIT(square);
@@ -84,6 +87,56 @@ static void init_king_attack_bitboard(Square square)
     king_attacks[square] |= (king_bitboard >> (LNX_BOARD_HEIGHT - 1)) & LNX_BITBOARD_NOT_FILE_A;
 }
 
+static void init_bishop_blocker_mask(Square square)
+{
+    Bitboard bishop_bitboard = BIT(square);
+    bishop_blocker_masks[square] = LNX_BITBOARD_EMPTY;
+
+    const File file = LNX_SQUARE_TO_FILE(square);
+    const Rank rank = LNX_SQUARE_TO_RANK(square);
+
+    uint8_t y = rank;
+    uint8_t x = file;
+    while(x < LNX_BOARD_WIDTH - 1 && y < LNX_BOARD_HEIGHT - 1)
+        bishop_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE(x++, y++));
+
+    y = rank;
+    x = file;
+    while(x < LNX_BOARD_WIDTH - 1 && y)
+        bishop_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE(x++, y--));
+
+    y = rank;
+    x = file;
+    while(x && y)
+        bishop_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE(x--, y--));
+
+    y = rank;
+    x = file;
+    while(x && y < LNX_BOARD_HEIGHT - 1)
+        bishop_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE(x--, y++));
+
+    bishop_blocker_masks[square] &= ~bishop_bitboard;
+}
+
+static void init_rook_blocker_mask(Square square)
+{
+    Bitboard rook_bitboard = BIT(square);
+    rook_blocker_masks[square] = LNX_BITBOARD_EMPTY;
+
+    const File file = LNX_SQUARE_TO_FILE(square);
+    const Rank rank = LNX_SQUARE_TO_RANK(square);
+
+    for(uint8_t i = 0; i < LNX_BOARD_WIDTH; ++i)
+    {
+        rook_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE((file + i) % LNX_BOARD_WIDTH, rank));
+        rook_blocker_masks[square] |= BIT(LNX_FILE_RANK_TO_SQUARE(file, (rank + i) % LNX_BOARD_HEIGHT));
+    }
+
+    rook_blocker_masks[square] &= ~(BIT(LNX_FILE_RANK_TO_SQUARE(file, LNX_RANK_1)) | BIT(LNX_FILE_RANK_TO_SQUARE(file, LNX_RANK_8)) | BIT(LNX_FILE_RANK_TO_SQUARE(LNX_FILE_A, rank)) | BIT(LNX_FILE_RANK_TO_SQUARE(LNX_FILE_H, rank)));
+
+    rook_blocker_masks[square] &= ~rook_bitboard;
+}
+
 void bitboard_init(void)
 {
     for(Square square = LNX_SQUARE_A1; square <= LNX_SQUARE_H8; ++square)
@@ -94,6 +147,9 @@ void bitboard_init(void)
         init_black_pawn_attack_bitboard(square);
         init_knight_attack_bitboard(square);
         init_king_attack_bitboard(square);
+
+        init_bishop_blocker_mask(square);
+        init_rook_blocker_mask(square);
     }
 }
 
